e18d7a5a6787a22e2feb44f50739f9f6f7ca93fe
Paul Kern <paul.kern@insa-strasbourg.fr>
2023-01-07 18:03:45 +0100

    Renamed tests

--- test_construct_numbers.py
+++ test_construct_numbers.py
@@ -4,7 +4,7 @@ from interpreter import Interpreter
 
 class TestPhi(unittest.TestCase):
 
-    def test_construct_number_positive(self):
+    def test_positive_numbers(self):
         interpreter = Interpreter()
 
         n1 = interpreter._construct_number([(0, 1)])
@@ -17,7 +17,7 @@ class TestPhi(unittest.TestCase):
         self.assertEqual(n9876_pos, 9876)
         self.assertEqual(n9876_neg, 9876)
 
-    def test_construct_number_negative(self):
+    def test_negative_numbers(self):
         interpreter = Interpreter()
 
         nm1 = interpreter._construct_number([(0, 0), (0, 1)])
@@ -30,21 +30,21 @@ class TestPhi(unittest.TestCase):
         self.assertEqual(nm9876_pos, -9876)
         self.assertEqual(nm9876_neg, -9876)
 
-    def test_construct_zero(self):
+    def test_zero(self):
         interpreter = Interpreter()
 
         zero = interpreter._construct_number([(0, 0)])
 
         self.assertEqual(zero, 0)
 
-    def test_construct_no_number(self):
+    def test_no_number(self):
         interpreter = Interpreter()
 
         zero = interpreter._construct_number([(1, 0)])
 
         self.assertEqual(zero, None)
 
-    def test_construct_number_more_deltas(self):
+    def test_more_deltas_after(self):
         interpreter = Interpreter()
 
         zero = interpreter._construct_number([(0, 0), (1, 0)])
@@ -55,7 +55,7 @@ class TestPhi(unittest.TestCase):
         self.assertEqual(ten, 10)
         self.assertEqual(minus_one, -1)
 
-    def test_construct_number_more_deltas(self):
+    def test_multiple_zeros(self):
         interpreter = Interpreter()
 
         zero = interpreter._construct_number([(0, 0), (0, 0), (1, 0)])
